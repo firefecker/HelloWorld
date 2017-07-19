d575271cd0eeb2a37c27c4d1ec4d470597889c50
hzhcoding <1606561454@qq.com>
2017-07-19 17:03:31 +0800

    创建md

---
+++ README.md
@@ -0,0 +1,7 @@
+#HelloWorld
+
+ReactNative 开发初体验
+
+react-native bundle --platform android --dev false --entry-file index.android.js  --bundle-output android/app/src/main/assets/index.android.bundle  --assets-dest android/app/src/main/res/
+
+react-native run-android
--- index.android.js
+++ index.android.js
@@ -34,6 +34,7 @@ export default class HelloWorld extends Component {
 
 // react-native bundle --platform android --dev false --entry-file index.android.js  --bundle-output android/app/src/main/assets/index.android.bundle  --assets-dest android/app/src/main/res/
 
+//react-native run-android
 
 const styles = StyleSheet.create({
   container: {
